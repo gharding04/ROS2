c446e99ef75eb6265c73aeb8d9de292d4e240d08
andrewburroughs <andrewburroughs@users.noreply.github.com>
2021-12-02 18:38:48 +0000

    deploy: 59b7c1b331a4712b314411a78acab3f2ff5b44cd

--- search/all_c.js
+++ search/all_c.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['reboot_0',['reboot',['../communication__node_8cpp.html#ac0a43b99fdf8950fb14112fd963881c3',1,'communication_node.cpp']]]
+  ['razorbotz_20rmc_202021_2d2022_20documentation_20home_20page_0',['Razorbotz RMC 2021-2022 Documentation Home Page',['../index.html',1,'']]],
+  ['reboot_1',['reboot',['../communication__node_8cpp.html#ac0a43b99fdf8950fb14112fd963881c3',1,'communication_node.cpp']]]
 ];
