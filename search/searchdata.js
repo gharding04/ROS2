f7780c88e4dbd139ee68072544c8e0f062916472
andrewburroughs <andrewburroughs@users.noreply.github.com>
2021-12-14 20:54:28 +0000

    deploy: d3eb2aae693e4a9bc0b3a91b9330902cb39b3eb2

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,9 +1,10 @@
 var indexSectionsWithContent =
 {
-  0: "abcdghijklmprstuvz",
+  0: "abcdghijklmprstuvwz",
   1: "cdims",
   2: "abchjklptvz",
-  3: "bdgijkprstuvz"
+  3: "bdgijkprstuvz",
+  4: "w"
 };
 
 var indexSectionNames =
@@ -11,7 +12,8 @@ var indexSectionNames =
   0: "all",
   1: "classes",
   2: "files",
-  3: "functions"
+  3: "functions",
+  4: "pages"
 };
 
 var indexSectionLabels =
@@ -19,6 +21,7 @@ var indexSectionLabels =
   0: "All",
   1: "Classes",
   2: "Files",
-  3: "Functions"
+  3: "Functions",
+  4: "Pages"
 };
 
