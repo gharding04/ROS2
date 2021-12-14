9e79ef5d8f382ec14d901d579e227f853f30c25c
andrewburroughs <andrewburroughs@users.noreply.github.com>
2021-12-14 20:41:28 +0000

    deploy: 1414a643ef4dea5e1095eacfa58e9bef34273fe7

--- search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['razorbotz_20rmc_202021_2d2022_20documentation_20home_20page_0',['Razorbotz RMC 2021-2022 Documentation Home Page',['../index.html',1,'']]]
-];
--- search/searchdata.js
+++ search/searchdata.js
@@ -3,8 +3,7 @@ var indexSectionsWithContent =
   0: "abcdghijklmprstuvz",
   1: "cdims",
   2: "abchjklptvz",
-  3: "bdgijkprstuvz",
-  4: "r"
+  3: "bdgijkprstuvz"
 };
 
 var indexSectionNames =
@@ -12,8 +11,7 @@ var indexSectionNames =
   0: "all",
   1: "classes",
   2: "files",
-  3: "functions",
-  4: "pages"
+  3: "functions"
 };
 
 var indexSectionLabels =
@@ -21,7 +19,6 @@ var indexSectionLabels =
   0: "All",
   1: "Classes",
   2: "Files",
-  3: "Functions",
-  4: "Pages"
+  3: "Functions"
 };
 
