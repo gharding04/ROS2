3e9eaca3d7126f86abf4d02ebf5ceb23f40bf5bf
andrewburroughs <andrewburroughs@users.noreply.github.com>
2022-02-12 02:47:08 +0000

    deploy: 93cfab48c0dbd78b7566d34c60cf0a7c988f5277

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['insert_0',['insert',['../communication__node_8cpp.html#aa1d1d7dc5b9267631230ec004aa791d8',1,'insert(float value, uint8_t *array):&#160;communication_node.cpp'],['../communication__node_8cpp.html#a136214bd6c2111a04dd81800b0855b98',1,'insert(int value, uint8_t *array):&#160;communication_node.cpp']]]
+  ['initsetspeed_0',['initSetSpeed',['../logic__node_8cpp.html#a8e8ab0346bf070fbeb647b182a54ce56',1,'logic_node.cpp']]],
+  ['insert_1',['insert',['../communication__node_8cpp.html#aa1d1d7dc5b9267631230ec004aa791d8',1,'insert(float value, uint8_t *array):&#160;communication_node.cpp'],['../communication__node_8cpp.html#a136214bd6c2111a04dd81800b0855b98',1,'insert(int value, uint8_t *array):&#160;communication_node.cpp']]]
 ];
