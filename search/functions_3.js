a775c096634e0a74462db8920d113421b599536a
andrewburroughs <andrewburroughs@users.noreply.github.com>
2022-03-15 06:34:50 +0000

    deploy: f692de984bb602d2f6936c6868bfcc7da10e791e

--- search/functions_3.js
+++ search/functions_3.js
@@ -4,6 +4,7 @@ var searchData=
   ['getcurrenta_1',['getCurrentA',['../classPowerDistributionPanel.html#a2a62d2a9aca9bf110bfa661d3b04a998',1,'PowerDistributionPanel']]],
   ['getcurrentb_2',['getCurrentB',['../classPowerDistributionPanel.html#a086f8d6f814c2118bcdfa601580a487f',1,'PowerDistributionPanel']]],
   ['getcurrentc_3',['getCurrentC',['../classPowerDistributionPanel.html#a3b3f2339b1083c762c7ac00f5e55f357',1,'PowerDistributionPanel']]],
-  ['getvoltage_4',['getVoltage',['../classPowerDistributionPanel.html#a29a4967626121a40e31b9d585383fba1',1,'PowerDistributionPanel']]],
-  ['gocallback_5',['goCallback',['../talon__node_8cpp.html#a96020b6787543d826dbba7ce88bf0d7c',1,'talon_node.cpp']]]
+  ['getparameter_4',['getParameter',['../talon__node_8cpp.html#a9fa86cd5e61e40487c91964c30a994a5',1,'getParameter(std::string parameterName, std::string initialValue):&#160;talon_node.cpp'],['../talon__node_8cpp.html#ac65fb8780f198b58ddd2c97239a56a3e',1,'getParameter(std::string parameterName, int initialValue, int type):&#160;talon_node.cpp']]],
+  ['getvoltage_5',['getVoltage',['../classPowerDistributionPanel.html#a29a4967626121a40e31b9d585383fba1',1,'PowerDistributionPanel']]],
+  ['gocallback_6',['goCallback',['../talon__node_8cpp.html#a96020b6787543d826dbba7ce88bf0d7c',1,'talon_node.cpp']]]
 ];
