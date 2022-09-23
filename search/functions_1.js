c94237ac5e8451693431960d91e1c9701aa1c8bc
RazorbotzAdmin <RazorbotzAdmin@users.noreply.github.com>
2022-09-23 16:55:33 +0000

    deploy: d7f31d81c292df16ae798b5c70f472fe71ca7abc

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['calibrate_0',['calibrate',['../classexcavation_1_1excavation__node_1_1ExcavationNode.html#ae2dfa58f71ba52137530b5213d0526c3',1,'excavation.excavation_node.ExcavationNode.calibrate(self, odrv, num)'],['../classexcavation_1_1excavation__node_1_1ExcavationNode.html#a8b95f1d775fbf0e0ddbe12bcdf7df104',1,'excavation.excavation_node.ExcavationNode.calibrate(self)']]]
+  ['calibrate_0',['calibrate',['../classexcavation_1_1excavation__node_1_1ExcavationNode.html#a8b95f1d775fbf0e0ddbe12bcdf7df104',1,'excavation::excavation_node::ExcavationNode']]],
+  ['calibratesingleboard_1',['calibrateSingleBoard',['../classexcavation_1_1excavation__node_1_1ExcavationNode.html#a1dbee42333aaa63f9b4865c53b922230',1,'excavation::excavation_node::ExcavationNode']]]
 ];
