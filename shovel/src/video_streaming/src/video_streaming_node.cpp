3f8e30dc3df8101344f396c3616331acb08c2b17
andrewburroughs <andrewburroughs17@gmail.com>
2024-04-24 19:30:22 -0500

    Changed video streaming to connect to server

--- shovel/src/video_streaming/src/video_streaming_node.cpp
+++ shovel/src/video_streaming/src/video_streaming_node.cpp
@@ -94,23 +94,9 @@ int main(int argc, char **argv){
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
     address.sin_port = htons( PORT ); 
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if (listen(server_fd, 3) < 0) { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    }
+    connect(server_fd, (struct sockaddr*)&address, sizeof(address)); 
 
     bytesRead = read(new_socket, buffer, 1024); 
-    send(new_socket, hello.c_str(), strlen(hello.c_str()), 0); 
-
     fcntl(new_socket, F_SETFL, O_NONBLOCK);
     
 
